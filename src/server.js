e6277e39f9cfaf403d5c6da5fdffb9185ce4fd59
Misha Busko <Misha.1996@ukr.net>
2025-09-14 14:14:23 +0300

    step 3 - step 6

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './db/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -19,11 +20,27 @@ export const setupServer = () => {
         }),
     );
 
-    // app.get('/', (req, res) => {
-    //     res.json({
-    //         message: 'Hello world!',
-    //     });
-    // });
+    app.get('/contacts', async (req, res, next) => {
+        const contacts = await getAllContacts();
+        res.status(200).json({
+            message: 'Successfully found contacts!',
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res, next) => {
+        const { contactId } = req.params;
+        const contact = await getContactById(contactId);
+        if (!contact) {
+            return res.status(404).json({
+                message: 'Contact not found',
+            });
+        }
+        res.status(200).json({
+            message: `Successfully found contact with id ${contactId}!`,
+            data: contact,
+        });
+    });
 
     app.use((req, res, next) => {
         res.status(404).json({
