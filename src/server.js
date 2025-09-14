783a81d4da629f636eb4a14919430eb3b37bf799
Misha Busko <Misha.1996@ukr.net>
2025-09-14 23:38:58 +0300

    Add status codes to JSON responses in contacts API

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ export const setupServer = () => {
     app.get('/contacts', async (req, res, next) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
             message: 'Successfully found contacts!',
             data: contacts,
         });
@@ -33,10 +34,12 @@ export const setupServer = () => {
         const contact = await getContactById(contactId);
         if (!contact) {
             return res.status(404).json({
+                status: 404,
                 message: 'Contact not found',
             });
         }
         res.status(200).json({
+            status: 200,
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         });
@@ -44,12 +47,14 @@ export const setupServer = () => {
 
     app.use((req, res, next) => {
         res.status(404).json({
+            status: 404,
             message: 'Not found',
         });
     });
 
     app.use((err, req, res, next) => {
         res.status(500).json({
+            status: 500,
             message: 'Something went wrong',
             error: err.message,
         });
