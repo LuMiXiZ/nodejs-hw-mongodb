7752ec1247ee91c8e9cd8cdb075477a3cb9d4171
Misha Busko <Misha.1996@ukr.net>
2025-09-21 23:41:56 +0300

    Refactor updateContact function to simplify return value and remove metadata handling

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -21,20 +21,14 @@ export const deleteContact = async (contactId) => {
 };
 
 export const updateContact = async (contactId, payload, options = {}) => {
-    const rawResult = await ContactsCollection.findOneAndUpdate(
+    const updatedContact = await ContactsCollection.findOneAndUpdate(
         { _id: contactId },
         payload,
         {
             new: true,
-            includeResultMetadata: true,
             ...options,
-        },
+        }
     );
 
-    if (!rawResult || !rawResult.value) return null;
-
-    return {
-        contact: rawResult.value,
-        isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-    };
+    return updatedContact;
 };
