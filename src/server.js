7d5d182ee4ec52e82e9d48d03156219441142025
Misha Busko <Misha.1996@ukr.net>
2025-10-20 22:17:29 +0300

    feat: add OpenAPI documentation and Swagger UI integration
    - Updated package.json to include build and preview scripts for API documentation.
    - Added OpenAPI specification in docs/openapi.yaml for the Contacts API.
    - Implemented Swagger UI middleware in src/middlewares/swaggerDocs.js to serve API documentation.
    - Created response schemas for various API responses in swagger/components/responses.
    - Defined contact and updateContact schemas in swagger/components/schemas.
    - Added API paths for contacts CRUD operations in swagger/paths/contacts.
    - Introduced redocly configuration in redocly.yaml for documentation generation.
    - Added index.html for custom documentation layout.

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -25,6 +26,8 @@ export const setupServer = () => {
     );
 
     app.use(router);
+
+    app.use('/api-docs', swaggerDocs());
 
     app.use(notFoundHandler);
 
